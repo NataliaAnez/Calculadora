ae51d5698cbe3ad3144a32c02bf9415d4994d5b0
Natalia Anez Safar <nataliaanezsafar@gmail.com>
2021-01-14 20:12:25 -0400

    Suma en Get y Post

---
+++ CalculadoraWEB/CalculadoraWEB/Properties/Controller/CalculadoraController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculadoraWEB.Properties.Controller
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class CalculadoraController : Controller
+    {
+        [HttpGet]
+        public int Suma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        [HttpPost]
+        public int Sumar([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 + num2;
+        }
+
+    }
+}
+
